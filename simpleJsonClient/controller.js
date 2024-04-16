468a623937095d41e6c554b8483554c5fc1ec6e0
Disco-Pickle <144940154+Disco-Pickle@users.noreply.github.com>
2024-04-16 17:24:03 +0200

    Frontend (#2)
    * First Frontend Step
    
    * Some attempt at a test environment
    
    * asdf

--- simpleJsonClient/controller.js
+++ simpleJsonClient/controller.js
@@ -1,170 +1,60 @@
-$(function () {
-
-    $("#searchResult").hide();
-
-    $("#btn_Search").click(function (e) {
-        $("#searchResult").hide();
-        loaddata($("#searchfield").val());
+$(function () 
+{
+    // Clicking the load button
+    $("#btnLoad").click(function() 
+    {
+        loadAppointments();
     });
 
-
+    // Loading the appointments
     let data = null;
-
-    function loaddata(searchterm) {
-
+    function loadAppointments() 
+    {
         console.log("Sending Ajax Request");
         data = null;
 
         // see: https://api.jquery.com/jQuery.ajax/
-        $.ajax({
+        $.ajax
+        ({
             type: "GET", // HTTP method to be used
             url: "../serviceHandler.php", // Target URL to which the HTTP request is sent
             cache: false, // false forces that requested pages are not cached by the browser
-            data: {method: "queryPersonByName", param: searchterm}, // Data to be sent to the server
+            data: {method: "getAppointments"}, // Data to be sent to the server
             dataType: "json", // Data type expected to be received from the server
-            success: function (response) { // Function to be executed if the HTTP request is successful
-
-                console.log("Ajax-Success", response);
+            success: function (response) // Function to be executed if the HTTP request is successful
+            {
+                console.log("Ajax succeded", response);
                 data = response;
 
-                // EXTENSION START
-                // Changes search result description
-                $("#resultDescription").replaceWith("<label for='noOfentries' class='form-label' id='resultDescription'>Persons under this name:</label>"); 
-
-                // Reads all entries for the provided search term from data
-                $("#persons").replaceWith("<p type='text' readonly class='form-control-plaintext' id='persons'></p>"); // Replaces previous .append with the default <p>
-                $.each(data, function(i, v) { // "For each line in data, do:"
-                    $("#persons").append("<p>First Name: " + v.firstname + " | Last Name: " + v.lastname + " | E-Mail: " + v.email + " | Phone number: " + v.phone + " | Department: " + v.department + "</p>");
+                $.each(data, function(i, appt) // For each appointment fetched, a checkbox item is appended to the list
+                {
+                    if(appt.expired === 0) // If the appointment has not expired, it is selectable
+                    {
+                        $("#appointments").append
+                        (
+                            "<li class='list-group-item'>" +
+                                "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + i + "'>" +
+                                "<label class='form-check-label' for='appointment" + i + "'>" + appt.day + "." + appt.month + "." + appt.year + ", " + appt.startTime + " - " + appt.endTime + "</label>" + 
+                            "</li>"
+                        );
+                    }
+                    else // Otherwise it is disabled and marked as "(Expired)"
+                    {
+                        $("#appointments").append
+                        (
+                            "<li class='list-group-item'>" +
+                                "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + i + "' disabled>" +
+                                "<label class='form-check-label' for='appointment" + i + "'>" + appt.day + "." + appt.month + "." + appt.year + ", " + appt.startTime + " - " + appt.endTime + "(Expired)</label>" + 
+                            "</li>"
+                        );
+                    }
+                    i++;
                 });
-                // EXTENSION END
-
-                // Displays number of lines in response
-                $("#noOfentries").val(response.length);
-
-                // Displays search results with a 1 second fade-in
-                $("#searchResult").show(1000);
             },
-            error: function() { // Function to be executed if the HTTP request fails
-                console.error("Error! Trying next search method...");
-
-                // EXTENSION START
-                $.ajax({
-                    type: "GET", // HTTP method to be used
-                    url: "../serviceHandler.php", // Target URL to which the HTTP request is sent
-                    cache: false, // false forces that requested pages are not cached by the browser
-                    data: {method: "queryPersonById", param: searchterm}, // Data to be sent to the server
-                    dataType: "json", // Data type expected to be received from the server
-                    success: function (response) { // Function to be executed if the HTTP request is successful
-
-                        console.log("Ajax-Success", response);
-                        data = response;
-
-                        // Changes search result description
-                        $("#resultDescription").replaceWith("<label for='noOfentries' class='form-label' id='resultDescription'>Persons under this ID:</label>");
-
-                        // Reads all entries for the provided search term from data
-                        $("#persons").replaceWith("<p type='text' readonly class='form-control-plaintext' id='persons'></p>"); // Replaces previous .append with the default <p>
-                        $.each(data, function(i, v) { // "For each line in data, do:"
-                            $("#persons").append("<p>First Name: " + v.firstname + " | Last Name: " + v.lastname + " | E-Mail: " + v.email + " | Phone number: " + v.phone + " | Department: " + v.department + "</p>");
-                        });
-
-                        // Displays number of lines in response
-                        $("#noOfentries").val(response.length);
-
-                        // Displays search results with a 1 second fade-in
-                        $("#searchResult").show(1000);
-                    },
-                    error: function() { // Function to be executed if the HTTP request fails
-                        console.error("Error! Trying next search method...");
-
-                        $.ajax({
-                            type: "GET", // HTTP method to be used
-                            url: "../serviceHandler.php", // Target URL to which the HTTP request is sent
-                            cache: false, // false forces that requested pages are not cached by the browser
-                            data: {method: "queryPersonByDept", param: searchterm}, // Data to be sent to the server
-                            dataType: "json", // Data type expected to be received from the server
-                            success: function (response) { // Function to be executed if the HTTP request is successful
-
-                                console.log("Ajax-Success", response);
-                                data = response;
-
-                                // Changes search result description
-                                $("#resultDescription").replaceWith("<label for='noOfentries' class='form-label' id='resultDescription'>Persons in this Department:</label>");
-
-                                // Replaces previous .append with the default <p>
-                                $("#persons").replaceWith("<p type='text' readonly class='form-control-plaintext' id='persons'></p>");
-
-                                // Reads all entries for the provided search term from data
-                                $.each(data, function(i, v) { // "For each line in data, do:"
-                                    $("#persons").append("<p>First Name: " + v.firstname + " | Last Name: " + v.lastname + " | E-Mail: " + v.email + " | Phone number: " + v.phone + " | Department: " + v.department + "</p>");
-                                });
-
-                                // Displays number of lines in response
-                                $("#noOfentries").val(response.length);
-
-                                // Displays search results with a 1 second fade-in
-                                $("#searchResult").show(1000);
-                            },
-                            error: function() { // Function to be executed if the HTTP request fails
-                                console.error("Error! Trying next search method...");
-
-                                $.ajax({
-                                    type: "GET", // HTTP method to be used
-                                    url: "../serviceHandler.php", // Target URL to which the HTTP request is sent
-                                    cache: false, // false forces that requested pages are not cached by the browser
-                                    data: {method: "queryPersonByPhone", param: searchterm}, // Data to be sent to the server
-                                    dataType: "json", // Data type expected to be received from the server
-                                    success: function (response) { // Function to be executed if the HTTP request is successful
-
-                                        console.log("Ajax-Success", response);
-                                        data = response;
-
-                                        // Changes search result description
-                                        $("#resultDescription").replaceWith("<label for='noOfentries' class='form-label' id='resultDescription'>Persons with this Phone Number:</label>");
-
-                                        // Replaces previous .append with the default <p>
-                                        $("#persons").replaceWith("<p type='text' readonly class='form-control-plaintext' id='persons'></p>");
-
-                                        // Reads all entries for the provided search term from data
-                                        $.each(data, function(i, v) { // "For each line in data, do:"
-                                            $("#persons").append("<p>First Name: " + v.firstname + " | Last Name: " + v.lastname + " | E-Mail: " + v.email + " | Phone number: " + v.phone + " | Department: " + v.department + "</p>");
-                                        });
-
-                                        // Displays number of lines in response
-                                        $("#noOfentries").val(response.length);
-
-                                        // Displays search results with a 1 second fade-in
-                                        $("#searchResult").show(1000);
-                                    },
-                                    error: function() { // Function to be executed if the HTTP request fails
-                                        console.error("Error! Giving up.");
-
-                                        // Output if none of the search functions produce results
-                                        // Changes search result description
-                                        $("#resultDescription").replaceWith("<label for='noOfentries' class='form-label' id='resultDescription'>No entries found</label>");
-
-                                        // Replaces previous .append with the default <p>
-                                        $("#persons").replaceWith("<p type='text' readonly class='form-control-plaintext' id='persons'></p>");
-
-                                        // Displays number of lines in response
-                                        $("#noOfentries").val(0);
-
-                                        // Displays search results with a 1 second fade-in
-                                        $("#searchResult").show(1000);
-                                    }            
-                                });
-                            }
-                        });
-
-                    }            
-                });
-                // EXTENSION END
+            error: function(response) // Function to be executed if the HTTP request fails
+            { 
+                console.error("ERROR", response);
             }            
         });
-
-        // $.ajax is an asynchronous call ... these line are logged usually *before* "Ajax-Success"
-        console.log("loaddata finished");
-        console.log("Data (still null ... AJAX call not yet finished)", data);
     }
-
 });
