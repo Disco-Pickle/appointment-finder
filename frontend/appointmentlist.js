16791ed430a32293851584311327461210d84793
DavidJohannesS <davidjohannesSD@gmail.com>
2024-04-26 18:43:29 +0200

    init

--- frontend/appointmentlist.js
+++ frontend/appointmentlist.js
@@ -12,49 +12,40 @@ $(function ()
     function loadAppointments() 
     {    
         // Data is fetched via AJAX calls
-        $.ajax
-        ({
-            type: "GET", // HTTP method to be used
-            url: "../backend/api/api.php", // Target URL to which the HTTP request is sent
-            cache: false, // false forces that requested pages are not cached by the browser
-            data: {"action": "getAppointment"}, // Data to be sent to the server
-            dataType: "json", // Data type expected to be received from the server
-            success: function(response) // Function to be executed if the HTTP request is successful
-            {
-                console.log("Ajax succeded", response);
-                data = response;
+// Function to fetch appointment data for a given ID
+function fetchAppointmentData(appointmentId) {
+    $.ajax({
+        url: "../backend/api/api.php",
+        type: "POST",
+        data: JSON.stringify({
+            action: "getAppointment",
+            appointmentId: appointmentId
+        }),
+        contentType: "application/json", // Set the content type to JSON
+        dataType: "json",
+        success: function(response) {
+            if (response) {
+                // Handle the response (e.g., append to the list of appointments)
+                console.log("Appointment data for ID " + appointmentId, response);
+                // Append the data to your list of appointments
+                // ...
+                // Continue fetching next appointment
+                fetchAppointmentData(appointmentId + 1);
+            } else {
+                // No valid response, stop fetching
+                console.log("No more appointments found.");
+            }
+        },
+        error: function(response) {
+            console.error("Error fetching appointment data for ID " + appointmentId, response);
+        }
+    });
+}
 
-                // The fetched appointments are appended to the appointments element
-                $.each(data, function(i, appt) // For each appointment fetched, a checkbox item is appended to the list
-                {
-                    if(appt.expired === 0) // If the appointment has not expired, it is selectable
-                    {
-                        $("#appointments").append
-                        (
-                            "<li class='list-group-item'>" +
-                                "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + i + "'>" +
-                                "<label class='form-check-label' for='appointment" + i + "'>" + appt.day + "." + appt.month + "." + appt.year + ", " + appt.startTime + " - " + appt.endTime + "</label>" + 
-                            "</li>"
-                        );
-                    }
-                    else // Otherwise it is disabled and marked as "(Expired)"
-                    {
-                        $("#appointments").append
-                        (
-                            "<li class='list-group-item'>" +
-                                "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + i + "' disabled>" +
-                                "<label class='form-check-label' for='appointment" + i + "'>" + appt.day + "." + appt.month + "." + appt.year + ", " + appt.startTime + " - " + appt.endTime + "(Expired)</label>" + 
-                            "</li>"
-                        );
-                    }
-                    i++;
-                });
-            },
-            error: function(response) // Function to be executed if the HTTP request fails
-            { 
-                console.error("ERROR", response);
-            }            
-        });
+
+// Start fetching appointments from ID 1
+fetchAppointmentData(1);
+
     }
 });
 
