dcca4eb5034cb57756a4b4b9f7237f5ab81fbd2e
DavidJohannesS <davidjohannesSD@gmail.com>
2024-04-26 18:11:00 +0200

    s

--- backend/api/api.php
+++ backend/api/api.php
@@ -1,5 +1,17 @@
 <?php
 require_once '../config/init.php';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Credentials: true');
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // Respond successfully:
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    exit;
+}
 
 // Get the HTTP method, path and input of the request
 $method = $_SERVER['REQUEST_METHOD'];
