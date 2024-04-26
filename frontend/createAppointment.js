374ebef9f0e923104646d0f54fb283331c6d6012
Disco-Pickle <s.sacher102@gmail.com>
2024-04-26 15:33:24 +0200

    createAppointments parses user input

--- frontend/createAppointment.js
+++ frontend/createAppointment.js
@@ -1,24 +1,50 @@
 $(function ()
 {
     // Adding more proposed dates
-    let dates = 1;
+    let amountDates = 1;
     $("#btnAddDate").click(function()
     {
-        dates++;
+        // Adds new date to form and increments amountDates
+        amountDates++;
         $("#options").append(
             "<div class='row'>" +
                 "<div class='col-3'>" +
-                    "<input type='date' class='form-control' name='date" + dates + "' id='date" + dates + "'>" +
+                    "<input type='date' class='form-control' name='day" + amountDates + "' id='day" + amountDates + "'>" +
                 "</div>" + 
                 "<div class='col-2'>" +
-                    "<input type='time' class='form-control' name='starttime" + dates + "' id='starttime" + dates + "'>" +
+                    "<input type='time' class='form-control' name='starttime" + amountDates + "' id='starttime" + amountDates + "'>" +
                 "</div>" +
                 "<div class='col-2'>" +
-                    "<input type='time' class='form-control' name='endtime" + dates + "' id='endtime" + dates +"'>" +
+                    "<input type='time' class='form-control' name='endtime" + amountDates + "' id='endtime" + amountDates +"'>" +
                 "</div>" +
             "</div>"
         )
-    })
+        console.log("Date added");
+    });
 
-    
-})
+    // Submitting the new appointment (saving it in the DB)
+    let name;
+    let author;
+    let dates = [];
+    $("#btnSubmit").click(function()
+    {
+        // Get appointment name
+        name = $("#name").val();
+        console.log("name = " + name);
+
+        // Get author
+        author = $("#author").val();
+        console.log("author = " + author);
+
+        // Get proposed dates
+        for(let i = 1; i <= amountDates; i++)
+        {
+            dates.push([
+                $("#day" + i).val(),
+                $("#starttime" + i).val(),
+                $("#endtime" + i).val()
+            ]);
+            console.log("dates[" + (i - 1) + "] =" + dates[i - 1]);
+        }
+    });
+});
