32b21b7ebfac24e1154aa63e1cbebe843e64b362
Disco-Pickle <s.sacher102@gmail.com>
2024-04-28 18:56:58 +0200

    appt dates only load when appt not expired

--- frontend/appointmentlist.js
+++ frontend/appointmentlist.js
@@ -38,6 +38,52 @@ $(function () {
                                     "<label class='form-check-label' for='appointment" + appt.id + "' id='appointmentLabel" + appt.id + "'>#" + appt.id + " | Name: " + appt.name + ", Author: " + appt.author + "</label>" + 
                                 "</li>"
                             );
+
+                            // Appends appointment dates of this appointment
+                            $.ajax({
+                                url: "../backend/api/api.php",
+                                type: "POST",
+                                data: 
+                                JSON.stringify({
+                                    action: "getAppointmentDates",
+                                    appointmentId: appt.id
+                                }),
+                                contentType: "application/json",
+                                dataType: "json",
+                                success: function(dates) 
+                                {
+                                    console.log("Dates for appointment #" + appt.id + ":", dates);
+                                    
+                                    // Create new list for this appointment's dates
+                                    $("#appointmentLabel" + appt.id).append
+                                    (
+                                        "<ul class='list-group' id='appointmentDates" + appt.id + "'</ul>"
+                                    );
+                
+                                    // Append everything to the list item
+                                    dates.forEach(function(date, i)
+                                    {
+                                        $("#appointmentDates" + appt.id).append
+                                        (
+                                            "<li class='list-group-item'>" + 
+                                                "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + appt.id + "date" + i + "'>" + 
+                                                "<label class='form-check-label' for='appointment" + appt.id + "date" + i + "'>" + date.day + ", " + date.starttime + " - " + date.endtime + "</label>" + 
+                                            "</li>"
+                                        );
+                                    });
+                                },
+                                error: function(dates) 
+                                {
+                                    console.error("Error fetching appointment dates:", dates);
+                                }
+                            });
+
+                            /*
+                            // Toggle behavior when appointment is clicked
+                            $("#appointmentLabel" + appt.id).on("click", function() 
+                            {
+                            $("#appointment" + appt.id + "date" + ???).toggle(); // Toggle visibility
+                            }); */
                         }
                         else // Otherwise it is disabled and marked as "(Expired)"
                         {
@@ -48,55 +94,7 @@ $(function () {
                                     "<label class='form-check-label' for='appointment" + appt.id + "' id='appointmentLabel" + appt.id + "'>#" + appt.id + " | Name: " + appt.name + ", Author: " + appt.author + " (Expired)</label>" + 
                                 "</li>"
                             );
-                        }
-                       
-                        // Appends appointment dates of this appointment
-                        /* appendAppointmentDates(appt.id); */
-                        $.ajax({
-                            url: "../backend/api/api.php",
-                            type: "POST",
-                            data: 
-                            JSON.stringify({
-                                action: "getAppointmentDates",
-                                appointmentId: appt.id
-                            }),
-                            contentType: "application/json",
-                            dataType: "json",
-                            success: function(dates) 
-                            {
-                                console.log("Dates for appointment #" + appt.id + ":", dates);
-                                
-                                // Create new list for this appointment's dates
-                                $("#appointmentLabel" + appt.id).append
-                                (
-                                    "<ul class='list-group' id='appointmentDates" + appt.id + "'</ul>"
-                                );
-            
-                                // Append everything to the list item
-                                dates.forEach(function(date, i)
-                                {
-                                    $("#appointmentDates" + appt.id).append
-                                    (
-                                        "<li class='list-group-item'>" + 
-                                            "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + appt.id + "date" + i + "'>" + 
-                                            "<label class='form-check-label' for='appointment" + appt.id + "date" + i + "'>" + date.day + ", " + date.starttime + " - " + date.endtime + "</label>" + 
-                                        "</li>"
-                                    );
-                                });
-                            },
-                            error: function(dates) 
-                            {
-                                console.error("Error fetching appointment dates:", dates);
-                            }
-                        });
-
-                        /*
-                        // Toggle behavior when appointment is clicked
-                        $("#appointmentLabel" + appt.id).on("click", function() 
-                        {
-                        $("#appointment" + appt.id + "date" + ???).toggle(); // Toggle visibility
-                        }); */
-                      
+                        }                      
                     });
                 }
                 else 
@@ -110,56 +108,4 @@ $(function () {
         });
     }
 });
-    /*
-    // Fetch appointment dates
-    function appendAppointmentDates(apptID) 
-    {
-        $.ajax({
-            url: "../backend/api/api.php",
-            type: "POST",
-            data: 
-            JSON.stringify({
-                action: "getAppointmentDates",
-                appointmentId: apptID
-            }),
-            contentType: "application/json",
-            dataType: "json",
-            success: function(dates) 
-            {
-                if (dates && dates.dates) 
-                {
-                    // Update UI with fetched dates (e.g., display in a modal)
-                    console.log("Dates for appointment #" + apptID + ":", dates.dates);
-                    
-                    // Create new list for this appointment's dates
-                    $("#appointmentLabel" + apptID).append
-                    (
-                        "<ul class='list-group' id='appointmentDates" + apptID + "'</ul>"
-                    );
-
-                    // Append everything to the list item
-                    dates.forEach(function(dates, i)
-                    {
-                        $("#appointmentDates" + apptID).append
-                        (
-                            "<li class='list-group-item'>" + 
-                                "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + apptID + "date" + i + "'>" + 
-                                "<label class='form-check-label' for='appointment" + apptID + "date" + i + "'>" + dates[i].day + ", " + dates[i].starttime + " - " + dates[i].endtime + "</label>" + 
-                            "</li>"
-                        );
-                    });
-
-                }
-                else 
-                {
-                    console.log("No dates found for appointment #" + apptID);
-                }
-            },
-            error: function(dates) 
-            {
-                console.error("Error fetching appointment dates:", dates);
-            }
-        });
-    }
-    */
 
