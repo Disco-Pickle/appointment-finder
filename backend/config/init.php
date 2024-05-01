918fc0f99e781dd1d59a6b5ed82a85d3a71a3fbf
DavidJohannesS <davidjohannesSD@gmail.com>
2024-05-01 13:41:32 +0200

    da

--- backend/config/init.php
+++ backend/config/init.php
@@ -14,7 +14,6 @@ error_reporting(E_ALL);
 // Include necessary files
 require_once '../utilities/Database.php';
 require_once '../models/Person.php';
-require_once '../controllers/UserController.php';
 require_once '../controllers/AppointmentController.php';
 require_once '../api/requestProcessor.php';
 // Add other necessary files here
