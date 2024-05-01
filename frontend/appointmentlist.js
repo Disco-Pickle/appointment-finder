b87eb617ba35b78b0cce36e7a56f50ebbbc13602
Disco-Pickle <s.sacher102@gmail.com>
2024-05-01 11:18:26 +0200

    adding Persons to dates works

--- frontend/appointmentlist.js
+++ frontend/appointmentlist.js
@@ -1,13 +1,15 @@
 $(function () {
     // Clicking the load button
-    $("#btnLoad").click(function() {
+    $("#btnLoad").click(function() 
+    {
         loadAppointments();
     });
 
     // Loading the appointments
     function loadAppointments() 
     {
-        $.ajax({
+        $.ajax
+        ({
             url: "../backend/api/api.php",
             type: "POST",
             data: 
@@ -18,7 +20,8 @@ $(function () {
             dataType: "json",
             success: function(appointments) 
             {
-                if (appointments) {
+                if (appointments) 
+                {
                     // Clear existing appointments (if there are any)
                     $("#appointments").empty();
 
@@ -38,14 +41,14 @@ $(function () {
                                         "<button class='btn btn-primary' type='button' data-bs-toggle='collapse' data-bs-target='#appointmentCollapse" + appt.id + "'>#" + appt.id + " | " + appt.name + " (by " + appt.author + ")</button>" +  
                                     "</p>" + 
                                     "<div class='collapse' id='appointmentCollapse" + appt.id + "'>" +  
-                                        "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + appt.id + "'>" +
-                                        "<label class='form-check-label' for='appointment" + appt.id + "' id='appointmentLabel" + appt.id + "'>#" + appt.id + " | " + appt.name + " (by " + appt.author + ")</label>" + 
+                                        "<label class='col-form-label' for='appointment" + appt.id + "' id='appointmentLabel" + appt.id + "'>#" + appt.id + " | " + appt.name + " (by " + appt.author + ")</label>" + 
                                     "</div>" + 
                                 "</li>"
                             );
 
                             // Appends appointment dates of this appointment
-                            $.ajax({
+                            $.ajax
+                            ({
                                 url: "../backend/api/api.php",
                                 type: "POST",
                                 data: 
@@ -66,25 +69,29 @@ $(function () {
                                     );
                 
                                     // Append everything to the list item
+                                    let amtDates = 0; // Counts amount of dates
                                     dates.forEach(function(date, i)
                                     {
                                         $("#appointmentDates" + appt.id).append
                                         (
                                             "<li class='list-group-item'>" + 
-                                                "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + appt.id + "date" + i + "'>" + 
+                                                "<input class='form-check-input me-1' type='checkbox' value='true' id='appointment" + appt.id + "date" + i + "' data-dateid='" + date.id + "'>" + 
                                                 "<label class='form-check-label' for='appointment" + appt.id + "date" + i + "'>" + date.day + ", " + date.starttime + " - " + date.endtime + "</label>" + 
                                             "</li>"
                                         );
+                                        amtDates++;
                                     });
 
-                                    // Appends input field for name to this appointment
+                                    // Appends input field for name and a button for confirming datees to this appointment
                                     $("#appointmentLabel" + appt.id).append
                                     (
                                         "<div class='form-floating'>" + 
                                             "<input type='text' class='form-control' name='attendeeName" + appt.id + "' id='attendeeName" + appt.id + "' placeholder='Your name'>" + 
                                             "<label for='attendeeName" + appt.id + "'>Your name</label>" + 
-                                        "</div>"
-                                    );
+                                        "</div>" +
+                                        "<hr>" + 
+                                        "<button class='btn btn-success btnConfirmDates' data-apptid='" + appt.id + "' data-amtdates='" + amtDates + "'>Confirm Dates</button>"
+                                    ); 
                                 },
                                 error: function(dates) 
                                 {
@@ -103,7 +110,7 @@ $(function () {
                             $("#appointments").append
                             (
                                 "<li class='list-group-item'>" +
-                                    "<input class='form-check-input me-1' type='checkbox' value='' id='appointment" + appt.id + "' disabled>" +
+                                    "<input class='form-check-input me-1' type='checkbox' value='1' id='appointment" + appt.id + "' disabled>" +
                                     "<label class='form-check-label' for='appointment" + appt.id + "' id='appointmentLabel" + appt.id + "'>#" + appt.id + " | " + appt.name + " (by " + appt.author + ", expired)</label>" + 
                                 "</li>"
                             );
@@ -115,10 +122,56 @@ $(function () {
                     console.log("No appointments found.");
                 }
             },
-            error: function(appointments) {
+            error: function(appointments) 
+            {
                 console.error("Error fetching appointments:", appointments);
             }
         });
     }
-});
 
+    // Clicking the confirm button
+    $(document).on("click", ".btnConfirmDates", function() // "btnConfirmDates" is defined as a class instead of as an id to allow attaching this function to all buttons without a loop
+    {
+        let apptID = $(this).data("apptid"); // The data attributes are used to get the necessary parameters for confirmDates
+        let amtDates = $(this).data("amtdates");
+        confirmDates(apptID, amtDates);
+    });
+
+    // Sending the dates associated with the name to the DB
+    function confirmDates(apptID, amtDates)
+    {
+        let person = $("#attendeeName" + apptID).val();
+
+        for(let i = 0; i < amtDates; i++)
+        {
+            // Get status of checkbox of current date (boolean)
+            let transmitDate = $("#appointment" + apptID + "date" + i).prop('checked');
+            
+            // If checkbox was checked, name is appended to the date entry in the DB
+            if(transmitDate)
+            {
+                $.ajax
+                ({
+                    url: "../backend/api/api.php",
+                    type: "POST",
+                    data: 
+                    JSON.stringify({
+                        action: "insertPerson",
+                        dateId: $("#appointment" + apptID + "date" + i).data("dateid"),
+                        persons: [person]
+                    }),
+                    contentType: "application/json",
+                    dataType: "json",
+                    success: function(response)
+                    {
+                        console.log("Persons added to specified dates", response);
+                    },
+                    error: function(response)
+                    {
+                        console.log("ERROR: Adding persons to specified dates failed", response);
+                    }
+                });
+            }
+        }
+    }
+});
